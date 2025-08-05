6782af7afc44f8ef5e7fc1b216ac9f0a83e2a43f
Ajay Quirk <ajay@quirky.codes>
2025-08-06 10:00:48 +1200

    feature: Finish core logic for yearly mean

--- src/main/java/co/nz/restorelab/NewCalculateYearlyMean.java
+++ src/main/java/co/nz/restorelab/NewCalculateYearlyMean.java
@@ -4,7 +4,10 @@ import org.geoserver.catalog.*;
 import org.geoserver.catalog.impl.DimensionInfoImpl;
 import org.geoserver.wps.gs.GeoServerProcess;
 import org.geotools.api.data.DataStore;
+import org.geotools.api.data.FeatureWriter;
 import org.geotools.api.data.SimpleFeatureSource;
+import org.geotools.api.data.Transaction;
+import org.geotools.api.feature.simple.SimpleFeature;
 import org.geotools.api.feature.simple.SimpleFeatureType;
 import org.geotools.api.filter.Filter;
 import org.geotools.api.filter.FilterFactory;
@@ -13,8 +16,13 @@ import org.geotools.api.referencing.FactoryException;
 import org.geotools.api.referencing.crs.CoordinateReferenceSystem;
 import org.geotools.api.referencing.operation.NoninvertibleTransformException;
 import org.geotools.api.referencing.operation.TransformException;
+import org.geotools.api.util.ProgressListener;
+import org.geotools.data.DefaultTransaction;
 import org.geotools.data.simple.SimpleFeatureCollection;
+import org.geotools.data.simple.SimpleFeatureIterator;
 import org.geotools.factory.CommonFactoryFinder;
+import org.geotools.feature.DefaultFeatureCollection;
+import org.geotools.feature.simple.SimpleFeatureBuilder;
 import org.geotools.feature.simple.SimpleFeatureTypeBuilder;
 import org.geotools.geometry.jts.ReferencedEnvelope;
 import org.geotools.process.ProcessException;
@@ -23,10 +31,12 @@ import org.geotools.process.factory.DescribeProcess;
 import org.geotools.process.factory.DescribeResult;
 import org.geotools.referencing.CRS;
 import org.locationtech.jts.geom.Polygon;
+import org.springframework.security.core.parameters.P;
 
 import java.io.IOException;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.List;
 
 @DescribeProcess(
         title = "NewCalculateYearlyMean",
@@ -40,7 +50,8 @@ public class NewCalculateYearlyMean implements GeoServerProcess {
 
     @DescribeResult(description = "Returns the layer name that was created/modified")
     public String execute(
-        @DescribeParameter(name = "Starting year") int year
+        @DescribeParameter(name = "Starting year") int year,
+        ProgressListener progressListener
     ) throws ProcessException {
         LayerInfo layerInfo = catalog.getLayerByName("restore-lab:smc_measurements");
 
@@ -72,8 +83,9 @@ public class NewCalculateYearlyMean implements GeoServerProcess {
         Expression timeAttrSMC = filterFactory.property("utc_time");
         Expression timeAttrCheck = filterFactory.property("time");
 
+        GridCalculator gridCalculator;
         try {
-            GridCalculator gridCalculator = new GridCalculator(800);
+            gridCalculator = new GridCalculator(800);
         } catch (FactoryException | NoninvertibleTransformException e) {
             throw new ProcessException("Error creating the GridCalculator", e);
         }
@@ -96,18 +108,53 @@ public class NewCalculateYearlyMean implements GeoServerProcess {
 
             // check to see if we already have data for this range computed.
             Filter timeFilterCheck = filterFactory.between(timeAttrCheck,  filterFactory.literal(startDate), filterFactory.literal(endDate));
-
+            SimpleFeatureCollection smcRange;
             try {
                 SimpleFeatureCollection range = meanLayer.getFeatures(timeFilterCheck);
                 if (!range.isEmpty()) {
                     continue;
                 }
+                // Get features for month from layer
+                Filter timeFilter = filterFactory.between(timeAttrSMC, filterFactory.literal(startDate), filterFactory.literal(endDate));
+                smcRange = featureSource.getFeatures(timeFilter);
             } catch (IOException e) {
                 throw new ProcessException("Error getting features in the mean layer check");
             }
 
-        }
+            // Aggregate layers
+            List<GridCell> grid = gridCalculator.aggregate(smcRange, progressListener);
+            // Make simple feature collection for writing
 
+            DefaultFeatureCollection collection = new DefaultFeatureCollection();
+            SimpleFeatureBuilder builder;
+            try {
+                builder = new SimpleFeatureBuilder(getLayerFeatureType());
+            } catch (FactoryException e) {
+                throw new ProcessException("Error in creating the feature builder", e);
+            }
+
+            try {
+                DataStore dataStore = (DataStore) storeInfo.getDataStore(null);
+                Transaction transaction = new DefaultTransaction("create");
+
+                try (FeatureWriter<SimpleFeatureType, SimpleFeature> writer = dataStore.getFeatureWriterAppend("smc_year_mean", transaction)) {
+                    int fid = 0;
+                    for (GridCell cell : grid) {
+                        builder.add(cell.getPolygon());
+                        builder.add(cell.average());
+                        builder.add(startDate);
+                        SimpleFeature feature = builder.buildFeature("fid-"+fid++);
+                        SimpleFeature newFeature = writer.next();
+
+                        newFeature.setAttributes(feature.getAttributes());
+                        writer.write();
+                    }
+                }
+                transaction.commit();
+            } catch (IOException e) {
+                throw new ProcessException("Error in getting dataStore to write to", e);
+            }
+        }
         return "Hey";
     }
 
